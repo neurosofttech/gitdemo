0a6eb89bb1d19dc710c00afb4c9ab9193501080a
Mohammad Haque <mohammadhaque898@gmail.com>
2022-04-24 12:37:07 -0400

    My second commit

--- src/main/java/ConvertIntToString.java
+++ src/main/java/ConvertIntToString.java
@@ -1,5 +1,9 @@
+import java.sql.SQLOutput;
+
 public class ConvertIntToString {
     public static void main(String args[]){
+
+        System.out.println("Hi , This is more code in QA Branch");
         int i=200;
         String s=String.valueOf(i);
         System.out.println(i+100);//300 because + is binary plus operator
--- src/main/java/ConvertStringToInt.java
+++ src/main/java/ConvertStringToInt.java
@@ -1,6 +1,6 @@
 public class ConvertStringToInt {
     public static void main(String[] args) {
-
+        System.out.println("Hi , This is more code in QA Branch");
         //Converting String into int using Integer.parseInt()
 
 
